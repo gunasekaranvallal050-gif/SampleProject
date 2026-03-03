225023c6406f3bb8cf8cbf531c25228274af304b
Gunasekaranvallal050 <gunasekaran.vallal050@gmail.com>
2026-03-03 11:43:45 +0530

    added few lines of codes

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitPracticeProject {
+}
---
+++ src/org/git/GitSample.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class GitSample {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is Main Method");
+		System.out.println("Git Storage Purpose");
+	}
+
+}
