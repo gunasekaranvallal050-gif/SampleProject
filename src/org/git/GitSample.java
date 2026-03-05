b69232aa047a7870859a5416b2d38a1dab96721a
Gunasekaranvallal050 <gunasekaran.vallal050@gmail.com>
2026-03-05 13:28:35 +0530

    Suresh added the code

--- src/org/git/GitSample.java
+++ src/org/git/GitSample.java
@@ -2,6 +2,12 @@ package org.git;
 
 public class GitSample {
 	
+	private void clone3() {
+		
+		System.out.println("Suresh coded is done.");
+
+	}
+	
 	
 	public static void main(String[] args) {
 		
